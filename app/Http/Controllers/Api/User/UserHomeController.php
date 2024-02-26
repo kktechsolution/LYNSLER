fef14dba6b6d09949cc930d88f68ecefd5cf4752
Fazil <fazilkazi7@gmail.com>
2024-02-26 16:44:49 +0530

    rate

--- app/Http/Controllers/Api/User/UserHomeController.php
+++ app/Http/Controllers/Api/User/UserHomeController.php
@@ -166,7 +166,6 @@ class UserHomeController extends Controller
             'email' => 'email|required|unique:users,email,' . auth::user()->id,
             'phone' => 'required|unique:users,phone,' . auth::user()->id,
             'gender' => 'required',
-            'address' => 'required',
 
 
         );
